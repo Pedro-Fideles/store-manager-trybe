3cd16c1265269ad1146586cdc22549359120fc13
Pedro Fideles <pedro.fideles162@gmail.vom>
2022-07-01 16:46:47 -0300

    feat(req. 8): testes e funções para listar vendas da camada models

--- models/Sales.js
+++ models/Sales.js
@@ -8,6 +8,43 @@ const create = async () => {
   return insertId;
 };
 
+const list = async () => {
+  const query = `
+    SELECT
+      s.id AS saleId,
+      s.date AS date,
+      sp.product_id AS productId,
+      sp.quantity
+    FROM StoreManager.sales s
+    INNER JOIN StoreManager.sales_products sp
+  `;
+
+  const [data] = await connection.execute(query);
+
+  return data;
+};
+
+const getById = async (id) => {
+  const query = `
+    SELECT
+      s.date AS date,
+      sp.product_id AS productId,
+      sp.quantity
+    FROM StoreManager.sales s
+    INNER JOIN StoreManager.sales_products sp
+    WHERE s.id = ?
+  `;
+  const params = [id];
+
+  const [data] = await connection.execute(query, params);
+
+  if (data.length === 0) return null;
+
+  return data;
+};
+
 module.exports = {
   create,
+  list,
+  getById,
 };
--- tests/unit/models/Sales.test.js
+++ tests/unit/models/Sales.test.js
@@ -24,4 +24,103 @@ describe('Na camada model de Sales:', () => {
       expect(response).to.be.equal(result);
     });
   });
+
+  describe('Ao listar vendas', () => {
+    const result = [
+      {
+        saleId: 1,
+        date: "2021-09-09T04:54:29.000Z",
+        productId: 1,
+        quantity: 2
+      },
+      {
+        saleId: 1,
+        date: "2021-09-09T04:54:54.000Z",
+        productId: 2,
+        quantity: 2
+      }
+    ];
+
+    before(async () => {
+      const execute = [result];
+
+      sinon.stub(connection, 'execute').resolves(execute);
+    });
+
+    after(async () => {
+      connection.execute.restore();
+    });
+
+    it('retorna um array', async () => {
+      const response = await Sales.list();
+
+      expect(response).to.be.an('array');
+    });
+
+    it('retorna as informações corretas', async () => {
+      const response = await Sales.list();
+
+      expect(response).to.deep.equal(result);
+    });
+  });
+
+  describe('Ao buscar uma venda pelo "id"', () => {
+    describe('e não existe a venda', () => {
+      const payload = 11;
+
+      before(async () => {
+        const execute = [[]];
+
+        sinon.stub(connection, 'execute').resolves(execute);
+      });
+
+      after(async () => {
+        connection.execute.restore();
+      });
+
+      it('retorna null', async () => {
+        const response = await Sales.getById(payload);
+
+        expect(response).to.be.null;
+      });
+    });
+
+    describe('e a venda é encontrada', () => {
+      const payload = 1;
+      const result = [
+        {
+          "date": "2021-09-09T04:54:29.000Z",
+          "productId": 1,
+          "quantity": 2
+        },
+        {
+          "date": "2021-09-09T04:54:54.000Z",
+          "productId": 2,
+          "quantity": 2
+        }
+      ];
+
+      before(async () => {
+        const execute = [result];
+
+        sinon.stub(connection, 'execute').resolves(execute);
+      });
+
+      after(async () => {
+        connection.execute.restore();
+      });
+
+      it('retorna um array', async () => {
+        const response = await Sales.getById(payload);
+
+        expect(response).to.be.an('array');
+      });
+
+      it('retorna as informações corretas', async () => {
+        const response = await Sales.getById(payload);
+
+        expect(response).to.deep.equal(result);
+      });
+    });
+  });
 });
